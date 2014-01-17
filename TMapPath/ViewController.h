45515d5aab1df23c515ae56e6dc4d796a1b84e7e
Chae Yungi <l8673901@gmail.com>
2014-01-17 14:00:33 +0900

    Initial Commit

---
+++ TMapPath/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TMapPath
+//
+//  Created by SDT-1 on 2014. 1. 17..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ TMapPathTests/TMapPathTests.m
@@ -0,0 +1,34 @@
+//
+//  TMapPathTests.m
+//  TMapPathTests
+//
+//  Created by SDT-1 on 2014. 1. 17..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TMapPathTests : XCTestCase
+
+@end
+
+@implementation TMapPathTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
